10d93ccd5f78e282232a9ba1740c41303376ef1d
Eneias da Fonseca Dias <edias.dias@terra.com.br>
2025-06-22 22:13:12 -0300

    Corrigindo caminho para secret Firebase no Render

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 def iniciar_firebase():
+    caminho_credencial = '/etc/secrets/firebase_key.json'
+
     if not firebase_admin._apps:
-        cred = credentials.Certificate('seu_arquivo_chave_firebase.json')
+        cred = credentials.Certificate(caminho_credencial)
         firebase_admin.initialize_app(cred)
+
     return firestore.client()
